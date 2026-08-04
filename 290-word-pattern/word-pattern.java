6f3fb7589aa02aa99e1f1825ad99dda74a8b6c30
abinaya9484-abi <abinaya9484@gmail.com>
2026-08-04 16:21:56 +0530

    Time: 1 ms (83.31%) | Memory: 42.6 MB (88.01%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) {
+            return false;
+        }
+
+        HashMap<Character, String> pToS = new HashMap<>();
+        HashMap<String, Character> sToP = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char p = pattern.charAt(i);
+            String word = words[i];
+
+            if (pToS.containsKey(p)) {
+                if (!pToS.get(p).equals(word))
+                  return false;
+            } else if (sToP.containsKey(word)) {
+                if (sToP.get(word) != p)
+                  return false;
+            } else {
+                pToS.put(p, word);
+                sToP.put(word, p);
+            }
+        }
+        return true;
+    }
+}
