eb4d59745342a72a163de7c0ce0db74ba9412790
abinaya9484-abi <abinaya9484@gmail.com>
2026-08-31 12:49:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (91.24%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int left = 0 ;
+        int right = n ;
+        int ans = 0 ;
+        while(left <= right){
+            int mid = left + (right - left)/2 ;
+            long coinsNeeded = (long) mid * ( mid + 1 ) / 2 ;
+            if(coinsNeeded <= n ){
+                ans = mid ;
+                left = mid + 1 ;
+            }else{
+                right = mid - 1 ;
+            }
+        }
+        return ans ;
+    }
+}
